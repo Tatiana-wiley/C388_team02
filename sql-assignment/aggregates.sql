e3b82261378fd9abf154d8e1e1f6a883474fc94a
David Acuff <acuffdavid@gmail.com>
2024-04-04 12:00:45 -0700

    David - aggregates done

--- sql-assignment/aggregates.sql
+++ sql-assignment/aggregates.sql
@@ -1,4 +1,5 @@
 -- Your name and cohort here
+-- David Acuff - C388
 
 /*
 Aggregate Queries
@@ -10,45 +11,68 @@ REQUIREMENT - Use a multi-line comment to paste the first 5 or fewer results und
 USE orderbook_activity_db;
 
 -- #1: How many users do we have?
-Sanjana
+-- Sanjana
 
 
 -- #2: List the username, userid, and number of orders each user has placed.
-David
+-- David
+SELECT `User`.userid UserID, `User`.uname Username, COUNT(`Order`.orderid) OrderCount FROM `User`
+LEFT OUTER JOIN `Order` ON `Order`.userid = `User`.userid
+GROUP BY Username, UserID;
+/*
+1	admin	3
+5	alice	8
+3	james	3
+4	kendra	5
+6	robert	5
+ROWS=7
+*/
 
 
 -- #3: List the username, symbol, and number of orders placed for each user and for each symbol. 
 -- Sort results in alphabetical order by symbol.
-Morphy
+-- Morphy
 
 
 
 -- #4: Perform the same query as the one above, but only include admin users.
-Tatiana
+-- Tatiana
 
 
 -- #5: List the username and the average absolute net order amount for each user with an order.
 -- Round the result to the nearest hundredth and use an alias (averageTradePrice).
 -- Sort the results by averageTradePrice with the largest value at the top.
-Sanjana
+-- Sanjana
 
 
 -- #6: How many shares for each symbol does each user have?
 -- Display the username and symbol with number of shares.
-David
+-- David
+SELECT `User`.uname Username, Product.symbol Symbol, COUNT(`Order`.shares) ShareCount from `User`
+LEFT OUTER JOIN `Order` ON `Order`.userid = `User`.userid
+JOIN Product ON Product.symbol = `Order`.symbol
+GROUP BY Username, Symbol;
+/*
+admin	WLY		1
+admin	GS		1
+admin	AAPL	1
+alice	A		5
+alice	SPY		1
+ROWS=19
+*/
 
 
 -- #7: What symbols have at least 3 orders?
-Morphy
+-- Morphy
 
 
 -- #8: List all the symbols and absolute net fills that have fills exceeding $100.
 -- Do not include the WLY symbol in the results.
 -- Sort the results by highest net with the largest value at the top.
-Tatiana
+-- Tatiana
 
 
 -- #9: List the top five users with the greatest amount of outstanding orders.
 -- Display the absolute amount filled, absolute amount ordered, and net outstanding.
 -- Sort the results by the net outstanding amount with the largest value at the top.
-Sanjana
+-- Sanjana
--- sql-assignment/joins.sql
+++ sql-assignment/joins.sql
@@ -1,4 +1,5 @@
 -- Your name and cohort here
+-- David Acuff - C388
 
 /*
 Join Queries
@@ -10,43 +11,53 @@ REQUIREMENT - Use a multi-line comment to paste the first 5 or fewer results und
 USE orderbook_activity_db;
 
 -- #1: Display the dateJoined and username for admin users.
-David
+-- David
+SELECT `User`.uname Username, `User`.dateJoined DateJoined FROM User
+JOIN UserRoles ON UserRoles.userid = `User`.userid
+JOIN Role ON Role.roleid = UserRoles.roleid
+WHERE Role.name = "admin";
+/*
+admin	2023-02-14 13:13:28
+wiley	2023-04-01 13:13:28
+alice	2023-03-15 19:16:21
+ROWS=3
+*/
 
 
 -- #2: Display each absolute order net (share*price), status, symbol, trade date, and username.
 -- Sort the results with largest the absolute order net (share*price) at the top.
 -- Include only orders that were not canceled or partially canceled.
-Morphy
+-- Morphy
 
 
 
 -- #3: Display the orderid, symbol, status, order shares, filled shares, and price for orders with fills.
 -- Note that filledShares are the opposite sign (+-) because they subtract from ordershares!
-Tatiana
+-- Tatiana
 
 
 -- #4: Display all partial_fill orders and how many outstanding shares are left.
 -- Also include the username, symbol, and orderid.
-Sanjana
+-- Sanjana
 
 
 -- #5: Display the orderid, symbol, status, order shares, filled shares, and price for orders with fills.
 -- Also include the username, role, absolute net amount of shares filled, and absolute net order.
 -- Sort by the absolute net order with the largest value at the top.
-David
+-- David
 
 
 -- #6: Display the username and user role for users who have not placed an order.
-Morphy
+-- Morphy
 
 
 -- #7: Display orderid, username, role, symbol, price, and number of shares for orders with no fills.
-Tatiana
+-- Tatiana
 
 
 -- #8: Display the symbol, username, role, and number of filled shares where the order symbol is WLY.
 -- Include all orders, even if the order has no fills.
-Sanjana
+-- Sanjana
 
 
 
