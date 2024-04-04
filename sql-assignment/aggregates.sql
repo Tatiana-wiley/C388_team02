a6e2c4aecfa4c7d644ad1e48e6e9f2589a4cf77c
sanjanasachar <sanjanasachar@gmail.com>
2024-04-04 15:05:12 -0400

    Query 1,5,9 done

--- sql-assignment/aggregates.sql
+++ sql-assignment/aggregates.sql
@@ -10,7 +10,8 @@ REQUIREMENT - Use a multi-line comment to paste the first 5 or fewer results und
 USE orderbook_activity_db;
 
 -- #1: How many users do we have?
-Sanjana
+SELECT COUNT(userid) AS num_users
+FROM User;
 
 
 -- #2: List the username, userid, and number of orders each user has placed.
@@ -30,7 +31,12 @@ Tatiana
 -- #5: List the username and the average absolute net order amount for each user with an order.
 -- Round the result to the nearest hundredth and use an alias (averageTradePrice).
 -- Sort the results by averageTradePrice with the largest value at the top.
-Sanjana
+SELECT u.uname AS username,
+       ROUND(AVG(ABS(o.shares * o.price)), 2) AS averageTradePrice
+FROM `Order` o
+JOIN `User` u ON o.userid = u.userid
+GROUP BY u.userid, u.uname
+ORDER BY averageTradePrice DESC;
 
 
 -- #6: How many shares for each symbol does each user have?
@@ -51,4 +57,19 @@ Tatiana
 -- #9: List the top five users with the greatest amount of outstanding orders.
 -- Display the absolute amount filled, absolute amount ordered, and net outstanding.
 -- Sort the results by the net outstanding amount with the largest value at the top.
-Sanjana
+SELECT 
+    u.uname AS username,
+    SUM(ABS(f.share * f.price)) AS absolute_filled_amount,
+    SUM(ABS(o.shares * o.price)) AS absolute_ordered_amount,
+    SUM(ABS(o.shares * o.price)) - SUM(ABS(f.share * f.price)) AS net_outstanding
+FROM 
+    `Fill` f
+JOIN 
+    `Order` o ON f.orderid = o.orderid
+JOIN 
+    `User` u ON o.userid = u.userid
+GROUP BY 
+    u.userid, u.uname
+ORDER BY 
+    net_outstanding DESC
+LIMIT 5;
