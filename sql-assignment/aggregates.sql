8d67b3dc8210d73891b7469ba550bdbae7f0e0ab
Morphy-Yeboah <62461275+Morphy-Yeboah@users.noreply.github.com>
2024-04-04 16:20:21 -0400

    Update aggregates.sql

--- sql-assignment/aggregates.sql
+++ sql-assignment/aggregates.sql
@@ -1,5 +1,5 @@
 
--- Your name and cohort here
+-- Morphy Yeboah c388
 
 /*
 Aggregate Queries
@@ -20,7 +20,22 @@ David
 
 -- #3: List the username, symbol, and number of orders placed for each user and for each symbol. 
 -- Sort results in alphabetical order by symbol.
-Morphy
+select u.uname, ord.symbol, count(u.uname) as orders_placed_user -- count(ord.symbol) as orders_placed_for_each_symbol
+from `User` as u
+join `Order` as ord on u.userid = ord.userid
+Group by u.uname, ord.symbol
+Order by ord.symbol asc;
+
+/*
+	alice	A	5
+	james	A	1
+	robert	AAA	1
+	admin	AAPL	1
+	robert	AAPL	1
+	kendra	AAPL	1
+    
+*/
+
 
 
 
@@ -40,7 +55,18 @@ David
 
 
 -- #7: What symbols have at least 3 orders?
-Morphy
+select count(ord.symbol) as orders_placed_for_each_symbol, ord.symbol
+from `Order` as ord
+join `User` as u on ord.userid = u.userid
+Group by ord.symbol
+having orders_placed_for_each_symbol >= 3;
+
+/*
+3	WLY
+3	AAPL
+6	A
+*/
+
 
 
 -- #8: List all the symbols and absolute net fills that have fills exceeding $100.
